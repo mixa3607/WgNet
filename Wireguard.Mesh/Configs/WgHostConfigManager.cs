6d64f70129f5154c63b4f7d7a8170b91839a208b
mixa3607 <30209772+mixa3607@users.noreply.github.com>
2022-02-16 00:20:47 +0500

    improves

--- Wireguard.Mesh/Configs/WgHostConfigManager.cs
+++ Wireguard.Mesh/Configs/WgHostConfigManager.cs
@@ -1,32 +1,48 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using System.IO;
 using ArkProjects.Wireguard.Mesh.JConverters;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace ArkProjects.Wireguard.Mesh.Configs
 {
-    public class WgHostConfigManager
+    public static class WgHostConfigManager
     {
         private static readonly JsonSerializerSettings JsonSerializerSettings = new JsonSerializerSettings()
         {
-            Converters = new List<JsonConverter> { new IPAddressConverter(), new IPNetworkConverter() },
+            Converters = new JsonConverter[] { new IPAddressConverter(), new IPNetworkConverter(), new StringEnumConverter() },
             DefaultValueHandling = DefaultValueHandling.Ignore,
             Formatting = Formatting.Indented
         };
 
+        /// <summary>
+        /// Load config fom file
+        /// </summary>
         public static WgHostConfig LoadFile(string filePath)
         {
+            if (filePath == null) 
+                throw new ArgumentNullException(nameof(filePath));
+
             var jsonStr = File.ReadAllText(filePath);
             return Load(jsonStr);
         }
 
+        /// <summary>
+        /// Load config from file
+        /// </summary>
         public static WgHostConfig Load(string jsonStr)
         {
             return JsonConvert.DeserializeObject<WgHostConfig>(jsonStr, JsonSerializerSettings);
         }
 
+        /// <summary>
+        /// Save config to file
+        /// </summary>
         public static void SaveAsFile(WgHostConfig config, string filePath, bool createPath = false)
         {
+            if (filePath == null) 
+                throw new ArgumentNullException(nameof(filePath));
+
             if (createPath)
             {
                 var dir = Path.GetDirectoryName(filePath);
@@ -36,6 +52,9 @@ namespace ArkProjects.Wireguard.Mesh.Configs
             File.WriteAllText(filePath, Save(config));
         }
 
+        /// <summary>
+        /// Convert config to json string
+        /// </summary>
         public static string Save(WgHostConfig config)
         {
             return JsonConvert.SerializeObject(config, JsonSerializerSettings);
--- Wireguard.Mesh/Configs/WgMeshConfigManager.cs
+++ Wireguard.Mesh/Configs/WgMeshConfigManager.cs
@@ -1,30 +1,39 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using ArkProjects.Wireguard.Mesh.JConverters;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace ArkProjects.Wireguard.Mesh.Configs
 {
-    public class WgMeshConfigManager
+    public static class WgMeshConfigManager
     {
-        private static readonly JsonSerializerSettings JsonSerializerSettings = new JsonSerializerSettings()
+        private static readonly JsonSerializerSettings JsonSerializerSettings = new()
         {
-            Converters = new List<JsonConverter> { new IPAddressConverter(), new IPNetworkConverter() },
+            Converters = new JsonConverter[] { new IPAddressConverter(), new IPNetworkConverter(), new StringEnumConverter() },
             DefaultValueHandling = DefaultValueHandling.Ignore,
             Formatting = Formatting.Indented
         };
 
+        /// <summary>
+        /// Load config fom file
+        /// </summary>
         public static WgMeshConfig LoadFile(string filePath)
         {
             var jsonStr = File.ReadAllText(filePath);
             return Load(jsonStr);
         }
 
+        /// <summary>
+        /// Load config from file
+        /// </summary>
         public static WgMeshConfig Load(string jsonStr)
         {
             return JsonConvert.DeserializeObject<WgMeshConfig>(jsonStr, JsonSerializerSettings);
         }
 
+        /// <summary>
+        /// Save config to file
+        /// </summary>
         public static void SaveAsFile(WgMeshConfig config, string filePath, bool createPath = false)
         {
             if (createPath)
@@ -36,6 +45,9 @@ namespace ArkProjects.Wireguard.Mesh.Configs
             File.WriteAllText(filePath, Save(config));
         }
 
+        /// <summary>
+        /// Convert config to json string
+        /// </summary>
         public static string Save(WgMeshConfig config)
         {
             return JsonConvert.SerializeObject(config, JsonSerializerSettings);
